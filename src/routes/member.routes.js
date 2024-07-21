a9502b7f9a79e7135fefae6072be13cd05926374
achint <achintj96244@gmail.com>
2024-07-21 13:18:32 +0530

    feat: Add getListOfMembers and getMemberById routes to member.routes.js

--- src/routes/member.routes.js
+++ src/routes/member.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { verifyClient } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { createMember } from "../controllers/member.controller.js";
+import {
+  createMember,
+  getListOfMembers,
+  getMemberById,
+} from "../controllers/member.controller.js";
 
 const router = Router();
 router.use(verifyClient);
 
 router.route("/add").post(upload.none(), createMember);
+router.route("/").get(getListOfMembers);
+router.route("/:id").get(getMemberById);
 
 export default router;
