16d77608512996657115d86cba4cc620ac9b05b2
achint <achintj96244@gmail.com>
2024-07-22 16:38:56 +0530

    feat: Add deleteMemberShip and updateMemberShip routes and controller functions

--- src/routes/memberShip.routes.js
+++ src/routes/memberShip.routes.js
@@ -6,6 +6,8 @@ import {
   getListOfMemberships,
   getMemberShipById,
   getMemberdetailsbyId,
+  deleteMemberShip,
+  updateMemberShip,
 } from "../controllers/membership.controller.js";
 
 const router = Router();
@@ -15,5 +17,7 @@ router.route("/create").post(upload.none(), createMemberShip);
 router.route("/").get(getListOfMemberships);
 router.route("/member/:id").get(getMemberdetailsbyId);
 router.route("/:id").get(getMemberShipById);
+router.route("/delete/:id").delete(deleteMemberShip);
+router.route("/update/:id").put(upload.none(), updateMemberShip);
 
 export default router;
