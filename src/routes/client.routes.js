04a01c528b14b02760fd4b914a1db06165373bf5
achint <achintj96244@gmail.com>
2024-07-26 15:34:12 +0530

    feat: Add deleteClient route to client.routes.js

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -7,6 +7,7 @@ import {
   logoutClient,
   registerClient,
   getListOfClient,
+  deleteClient,
 } from "../controllers/auth.controller.js";
 
 const router = Router();
@@ -16,5 +17,6 @@ router.route("/").get(getListOfClient);
 router.route("/register").post(upload.none(), registerClient);
 router.route("/login").post(loginClient);
 router.route("/logout").post(verifyClient, logoutClient);
+router.route("/delete/:id").delete(verifyClient, deleteClient);
 
 export default router;
