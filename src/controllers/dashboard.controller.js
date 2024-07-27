c0f549812f088b5ec2093613997c0ac4e54d7a59
achint <achintj96244@gmail.com>
2024-07-27 13:44:27 +0530

    Resolved merge conflicts

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,35 +1,85 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+<<<<<<< HEAD
 import { MemberShip } from "../models/memberShip.model.js";
+=======
+import { MemberShip } from "../models/membership.model.js";
+import { Member } from "../models/member.model.js";
+>>>>>>> 8cc9d05 (refactor: Update dashboard controller to calculate additional metrics)
 
 const dashboard = asyncHandler(async (req, res, next) => {
+  const members = await Member.find({ clientId: req.client._id });
   const memberShips = await MemberShip.find({});
 
+  const memberShipList = members.map((member) => {
+    return memberShips.filter((memberShip) =>
+      memberShip.member.equals(member._id)
+    );
+  });
+
+  const flattenedMemberShipList = memberShipList.flat();
+
   let currentDate = new Date();
-  const monthly = currentDate.setMonth(currentDate.getMonth() - 1);
-  const weekly = currentDate.setDate(currentDate.getDate() - 7);
+  const monthlyDate = new Date(currentDate);
+  monthlyDate.setMonth(currentDate.getMonth() - 1);
+  const weeklyDate = new Date(currentDate);
+  weeklyDate.setDate(currentDate.getDate() - 7);
 
   let monthlyAmount = 0;
   let weeklyAmount = 0;
   let totalMontlyAmount = 0;
+  let activeMembersCount = 0;
+  let inactiveMembersCount = 0;
+  let newMembersCount = 0;
+  let expiredMembershipsCount = 0;
 
-  memberShips.forEach((memberShip) => {
+  const newMembersDate = new Date(currentDate);
+  newMembersDate.setDate(currentDate.getDate() - 30); // Example: new members in the last 30 days
+  const upcomingExpirationDate = new Date(currentDate);
+  upcomingExpirationDate.setDate(currentDate.getDate() + 30); // Example: memberships expiring in the next 30 days
+
+  members.forEach((member) => {
+    if (!member.overdue) {
+      activeMembersCount++;
+    } else {
+      inactiveMembersCount++;
+    }
+
+    const createdAtDate = new Date(member.createdAt);
+    if (createdAtDate > newMembersDate) {
+      newMembersCount++;
+    }
+  });
+
+  flattenedMemberShipList.forEach((memberShip) => {
     const startDateObject = new Date(
       memberShip.startDate.split("-").reverse().join("-")
     );
-    if (startDateObject > monthly) {
+    const endDateObject = new Date(
+      memberShip.endDate.split("-").reverse().join("-")
+    );
+
+    if (startDateObject > monthlyDate) {
       monthlyAmount += memberShip.paidAmount;
     }
-    if (startDateObject > weekly) {
+    if (startDateObject > weeklyDate) {
       weeklyAmount += memberShip.paidAmount;
     }
     totalMontlyAmount += memberShip.paidAmount;
+
+    if (endDateObject < currentDate) {
+      expiredMembershipsCount++;
+    }
   });
 
   const newdashboard = {
-    montlyCollection: monthlyAmount,
+    monthlyCollection: monthlyAmount,
     weeklyCollection: weeklyAmount,
     totalCollection: totalMontlyAmount,
+    activeMembers: activeMembersCount,
+    inactiveMembers: inactiveMembersCount,
+    newMembers: newMembersCount,
+    expiredMemberships: expiredMembershipsCount,
   };
 
   res.status(200).json(new ApiResponse(200, newdashboard, "dashboard info"));
