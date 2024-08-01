d44313a7b4c2890fc189b121f381bebab9972cf5
achint <achintj96244@gmail.com>
2024-08-01 23:55:28 +0530

    feat: Add membership dropdown model

---
+++ src/models/membershipDropDown.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const membershipDropDownSchema = new mongoose.Schema(
+  {
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Client",
+      required: true,
+    },
+    memberShipName: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const membershipDropDown = mongoose.model(
+  "membershipDropDown",
+  membershipDropDownSchema
+);
